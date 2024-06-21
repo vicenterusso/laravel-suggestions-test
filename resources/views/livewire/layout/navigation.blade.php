c49ece476a7ef56cfe335a82ee3bc65391cc028d
Vicente Russo <vicente.russo@gmail.com>
2024-06-21 19:53:00 -0300

    Lists and creates on dashboard

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -30,8 +30,8 @@ new class extends Component
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                        Sugestões
                     </x-nav-link>
                 </div>
             </div>
