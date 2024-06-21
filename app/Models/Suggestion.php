b27e1d210b805a00e0c9682e678885a7e2448715
Vicente Russo <vicente.russo@gmail.com>
2024-06-21 19:31:18 -0300

    Auto sets user logged it to the author of the suggetsion

--- app/Models/Suggestion.php
+++ app/Models/Suggestion.php
@@ -9,4 +9,16 @@ class Suggestion extends Model
 {
     use HasFactory;
 
+    public static function boot()
+    {
+        parent::boot();
+
+        // Evento de criação de sugestão, setando
+        // automaticamente o ID do usuário logado
+        static::creating(function ($model) {
+            if(auth()->check()) {
+                $model->user_id = auth()->user()->id;
+            }
+        });
+    }
 }
